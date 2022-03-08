a92bab8684afc1c56612c23a07a327c0ab33bb0e
weihaoysgs <weihao_ysgs@163.com>
2022-03-08 20:20:00 +0800

    添加定时器任务

---
+++ User/Tasks/include/pwtwo_task.h
@@ -0,0 +1,8 @@
+#ifndef _PSTWO_TASK_H_
+#define _PSTWO_TASK_H_
+
+#include "cmsis_os.h"
+#include "stm32f1xx.h"
+#include "stm32f1xx_ll_gpio.h"
+
+#endif //_PSTWO_TASK_H_
---
+++ User/Tasks/src/pwtwo_task.c
@@ -0,0 +1,13 @@
+#include "pwtwo_task.h"
+
+
+void StartPstwoTask(void const * argument)
+{
+
+    for(;;)
+    {
+        uint8
+        osDelay(10);
+    }
+}
+
