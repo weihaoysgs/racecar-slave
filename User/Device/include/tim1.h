5cb20bf12a0f17d48ad985f63b390b6f457559bd
Bashpow <B31296637828@hotmail.com>
2022-03-28 00:36:00 +0800

    删除所有文件

--- User/Device/include/tim1.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIM1_H_
-#define _TIM1_H_
-
-#include "stm32f1xx.h"
-#include "tim.h"
-
-void TimeOneInit(void);
-
-#endif //_TIM1_H_
--- User/Device/include/tim8.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIM8_H_
-#define _TIM1_H_
-
-#include "stm32f1xx.h"
-#include "tim.h"
-
-void TimeEightInit(void);
-
-#endif //_TIM1_H_
