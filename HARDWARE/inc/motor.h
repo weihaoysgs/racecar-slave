41b9246ad643ef6db9309944efe19fc2392c1a38
weihaoysgs <weihao_ysgs@163.com>
2022-03-28 19:46:01 +0800

    加入电机驱动支持

---
+++ HARDWARE/inc/motor.h
@@ -0,0 +1,9 @@
+#ifndef __MOTOR_H_
+#define __MOTOR_H_
+
+#include "stm32f10x.h"
+
+void SetMotorLeftPower(int32_t power);
+void SetMotorRightPower(uint16_t power);
+
+#endif // __MOTOR_H_
---
+++ HARDWARE/src/motor.c
@@ -0,0 +1,29 @@
+#include "motor.h"
+
+// U1
+// PC6 - IN2 - TIM8_CH1
+// PC7 - IN1 - TIM8_CH2
+
+// U2
+// PC8 - IN2 - TIM8_CH3
+// PC9 - IN1 - TIM8_CH4
+
+void SetMotorLeftPower(int32_t power) {
+	if(power >= 0) {
+		TIM8->CCR1 = 0;
+		TIM8->CCR2 = power;
+	} else {
+		TIM8->CCR1 = power;
+		TIM8->CCR2 = 0;
+	}
+}
+
+void SetMotorRightPower(uint16_t power) {
+	if(power >= 0) {
+		TIM8->CCR3 = power;
+		TIM8->CCR4 = 0;
+	} else {
+		TIM8->CCR3 = 0;
+		TIM8->CCR4 = power;
+	}
+}
