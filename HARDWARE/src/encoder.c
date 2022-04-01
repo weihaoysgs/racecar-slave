ffc4d5e5b96c763d267c47b19fb01c626f4d0474
Bashpow <B31296637828@hotmail.com>
2022-04-01 15:18:32 +0800

    小修改编码器

--- HARDWARE/src/encoder.c
+++ HARDWARE/src/encoder.c
@@ -1,7 +1,7 @@
 #include "encoder.h"
 
 /* --- 左电机编码器 BEGIN --- */
-static int8_t tim2_update_direction = 0;
+// static int8_t tim2_update_direction = 0;
 static int32_t tim2_last_cnt_value;
 
 /**
@@ -12,31 +12,35 @@ static int32_t tim2_last_cnt_value;
 int32_t GetTim2EncoderChangedValue(void) {
 	int32_t encoder_changed_value = 0;
 	int32_t tim2_this_cnt_value = TIM2->CNT;
-	encoder_changed_value = tim2_this_cnt_value-tim2_last_cnt_value + 3300*tim2_update_direction;
-	if (encoder_changed_value < -2500)
+	// encoder_changed_value = tim2_this_cnt_value-tim2_last_cnt_value + 3300*tim2_update_direction;
+	encoder_changed_value = tim2_this_cnt_value-tim2_last_cnt_value;
+	if (encoder_changed_value < -1650) {
 		encoder_changed_value += 3300;
-	if (encoder_changed_value > 2500)
+	}
+	if (encoder_changed_value > 1650) {
 		encoder_changed_value -= 3300;
-	tim2_update_direction = 0;
+	}
+	// tim2_update_direction = 0;
 	tim2_last_cnt_value = tim2_this_cnt_value;
 	return encoder_changed_value;
 }
 
 void TIM2_IRQHandler(void) {
-	if(TIM_GetITStatus(TIM2, TIM_IT_Update)) {
-		if(0u == ((TIM2->CR1) & ((0x1UL << (4U))))) {
-			tim2_update_direction++;
-		} else {
-			tim2_update_direction--;
-		}
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-	}
+	// !!! Do Nothing
+	// if(TIM_GetITStatus(TIM2, TIM_IT_Update)) {
+	// 	if(0u == ((TIM2->CR1) & ((0x1UL << (4U))))) {
+	// 		tim2_update_direction++;
+	// 	} else {
+	// 		tim2_update_direction--;
+	// 	}
+	// 	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+	// }
 }
 /* --- 左电机编码器 END --- */
 
 
 /* --- 右电机编码器 BEGIN --- */
-static int8_t tim3_update_direction = 0;
+// static int8_t tim3_update_direction = 0;
 static int32_t tim3_last_cnt_value;
 
 /**
@@ -47,24 +51,28 @@ static int32_t tim3_last_cnt_value;
 int32_t GetTim3EncoderChangedValue(void) {
 	int32_t encoder_changed_value = 0;
 	int32_t tim3_this_cnt_value = TIM3->CNT;
-	encoder_changed_value = tim3_this_cnt_value-tim3_last_cnt_value + 3300*tim3_update_direction;
-	if (encoder_changed_value < -2500)
+	// encoder_changed_value = tim3_this_cnt_value-tim3_last_cnt_value + 3300*tim3_update_direction;
+	encoder_changed_value = tim3_this_cnt_value-tim3_last_cnt_value;
+	if (encoder_changed_value < -2500) {
 		encoder_changed_value += 3300;
-	if (encoder_changed_value > 2500)
+	}
+	if (encoder_changed_value > 2500) {
 		encoder_changed_value -= 3300;
-	tim3_update_direction = 0;
+	}
+	// tim3_update_direction = 0;
 	tim3_last_cnt_value = tim3_this_cnt_value;
 	return encoder_changed_value;
 }
 
 void TIM3_IRQHandler(void) {
-	if(TIM_GetITStatus(TIM3, TIM_IT_Update)) {
-		if(0u == ((TIM3->CR1) & ((0x1UL << (4U))))) {
-			tim3_update_direction++;
-		} else {
-			tim3_update_direction--;
-		}
-		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-	}
+	// !!! Do Nothing
+	// if(TIM_GetITStatus(TIM3, TIM_IT_Update)) {
+	// 	if(0u == ((TIM3->CR1) & ((0x1UL << (4U))))) {
+	// 		tim3_update_direction++;
+	// 	} else {
+	// 		tim3_update_direction--;
+	// 	}
+	// 	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+	// }
 }
 /* --- 右电机编码器 END --- */
