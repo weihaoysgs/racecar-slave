4944ceeba8b2079dafd94e5da7eff678a9373a3c
Bashpow <B31296637828@hotmail.com>
2022-04-13 11:38:43 +0800

    完成上传数据

--- LIBS/inc/upload_message.h
+++ LIBS/inc/upload_message.h
@@ -3,13 +3,19 @@
 
 #include "stm32f10x.h"
 
-typedef __packed struct 
+typedef __packed struct
 {
-    uint8_t package_head;
-    uint8_t cmd_id;
-    uint8_t count;
-    
-    // struct{
+    // 帧头
+    __packed struct
+    {
+        uint8_t package_head;
+        uint8_t cmd_id;
+        uint8_t count;
+    } frame_head;
+
+    // 数据段
+    __packed struct
+    {
         int16_t left_speed;
         int16_t right_speed;
 
@@ -19,11 +25,11 @@ typedef __packed struct
         uint16_t servo_angle;
 
         float yaw_angle;
-    // }__attribute__((packed)) data;
+    } data;
 
-    uint8_t crc8_itu;
-
-}Send_message_t;
+    //帧尾 crc8校验
+    uint8_t crc8;
+} Send_message_t;
 
 void Set_Ros_Send_Data_MSG(void);
 uint32_t Get_Ros_Send_Data_Len(void);
--- LIBS/src/upload_message.c
+++ LIBS/src/upload_message.c
@@ -5,18 +5,18 @@ static Send_message_t send_message;
 
 void Set_Ros_Send_Data_MSG(void)
 {
-    send_message.package_head = 0xA5;
-    send_message.cmd_id = 0x11;
-    send_message.count++;
+    send_message.frame_head.package_head = 0xA5;
+    send_message.frame_head.cmd_id = 0x11;
+    send_message.frame_head.count++;
 
-    send_message.left_speed = GetMotorLeftSpeed();
-    send_message.right_speed = GetMotorRightSpeed();
-    send_message.left_count = GetMotorLeftCount();
-    send_message.right_count = GetMotorRightCount();
-    send_message.servo_angle = TIM1->CCR1;
-    send_message.yaw_angle = 0.0f;
+    send_message.data.left_speed = GetMotorLeftSpeed();
+    send_message.data.right_speed = GetMotorRightSpeed();
+    send_message.data.left_count = GetMotorLeftCount();
+    send_message.data.right_count = GetMotorRightCount();
+    send_message.data.servo_angle = TIM1->CCR1;
+    send_message.data.yaw_angle = 0.0f;
 
-    send_message.crc8_itu = 0xFE;
+    send_message.crc8 = 0xFE;
 }
 
 uint32_t Get_Ros_Send_Data_Len(void)
