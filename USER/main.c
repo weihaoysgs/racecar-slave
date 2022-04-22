422807ba9e9727e52f31222ca44fb4dcebe80d80
Bashpow <B31296637828@hotmail.com>
2022-04-22 14:07:17 +0800

    完善主函数权限

--- USER/main.c
+++ USER/main.c
@@ -10,30 +10,44 @@
 
 int main(void)
 {
+	// 进行MCU初始化
+	{
+		// 中断锁
+		rt_base_t level = rt_hw_interrupt_disable();
 
-	// 外设初始化
-	Peripheral_Init();
+		// 外设初始化
+		Peripheral_Init();
 
-	// BEGIN 初始化并启动任务 BEGIN
-	Motor_Speed_Clc_Timer_Init();
-	Pstwo_Thread_Init();
-	Chassis_Thread_Init();
-	Ahrs_Thread_Init();
-	Rosmsg_Thread_Init();
-	Upload_Thread_Init();
-	Shell_Thread_Init();
+		// BEGIN 初始化并启动任务 BEGIN
+		Motor_Speed_Clc_Timer_Init();
+		Pstwo_Thread_Init();
+		Chassis_Thread_Init();
+		Ahrs_Thread_Init();
+		Rosmsg_Thread_Init();
+		Upload_Thread_Init();
+		Shell_Thread_Init();
 
-	rt_timer_start(Get_Motor_Speed_Clc_Timer_Object());
-	rt_thread_startup(Get_Chassis_Thread_Object());
-	rt_thread_startup(Get_Pstwo_Thread_Object());
-	rt_thread_startup(Get_Ahrs_Thread_Object());
-	rt_thread_startup(Get_Rosmsg_Thread_Object());
-	rt_thread_startup(Get_Upload_Thread_Object());
-	rt_thread_startup(Get_Shell_Thread_Object());
-	// END 初始化并启动任务 END
+		rt_timer_start(Get_Motor_Speed_Clc_Timer_Object());
+		rt_thread_startup(Get_Chassis_Thread_Object());
+		rt_thread_startup(Get_Pstwo_Thread_Object());
+		rt_thread_startup(Get_Ahrs_Thread_Object());
+		rt_thread_startup(Get_Rosmsg_Thread_Object());
+		rt_thread_startup(Get_Upload_Thread_Object());
+		rt_thread_startup(Get_Shell_Thread_Object());
+		// END 初始化并启动任务 END
 
+		// 解除中断锁
+		rt_hw_interrupt_enable(level);
+
+		// 降低 MAIN_THREAD 的优先级
+		uint8_t main_priority = 7u;
+		rt_thread_control(rt_thread_self(), RT_THREAD_CTRL_CHANGE_PRIORITY, &main_priority);
+	}
+
+	// 定时闪灯
 	for (;;)
 	{
-		rt_thread_delay(500);
+		LED_TOGGLE();
+		rt_thread_delay(800);
 	}
 }
